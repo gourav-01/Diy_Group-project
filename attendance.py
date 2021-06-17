94d59a4d9b4b99d93fb876c7c7bc0ef46f8c9cef
Gourav Kumar Shaw <73769604+gourav-01@users.noreply.github.com>
2021-06-17 17:09:28 +0530

    Add files via upload

---
+++ attendance.py
@@ -0,0 +1,118 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+import pyttsx3
+import serial
+import time
+# from PIL import ImageGrab
+ 
+path = r'C:\Users\goura\Desktop\DIY_Week7_Group Project\ImagesAttendance'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+
+print(classNames)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty("voice", voices[0].id)
+engine.setProperty("rate", 140)
+engine.setProperty("volume", 1000)
+ 
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+ 
+def markAttendance(name):
+    with open(r'C:\Users\goura\Desktop\DIY_Week7_Group Project\attendance01.csv','r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S\n')
+            f.writelines(f'n{name},{dtString}')
+ 
+#### FOR CAPTURING SCREEN RATHER THAN WEBCAM
+# def captureScreen(bbox=(300,300,690+300,530+300)):
+#     capScr = np.array(ImageGrab.grab(bbox))
+#     capScr = cv2.cvtColor(capScr, cv2.COLOR_RGB2BGR)
+#     return capScr
+ 
+encodeListKnown = findEncodings(images)
+print('Encoding Complete')
+ 
+cap = cv2.VideoCapture(0)
+ 
+while True:
+    success, img = cap.read()
+    #img = captureScreen()
+    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+ 
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS,facesCurFrame)
+ 
+    for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
+        #print(faceDis)
+        matchIndex = np.argmin(faceDis)
+ 
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            print(name)
+            time.sleep(3)
+
+            y1,x2,y2,x1 = faceLoc
+            y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+            markAttendance(name)
+            cv2.imshow('Webcam',img)
+            cv2.waitKey(1)
+            time.sleep(5)
+            speak("Face is matching with database. Attendance marked Welcome"+name)
+            ard = serial.Serial('com3', 9600)
+            time.sleep(2)
+            var = 'a'
+            c = var.encode()
+            speak("Please enter your fingerprint for autentication")
+
+            #finger-print checking code to be added here
+            
+            #ard.write(c)
+            #time.sleep(5)
+            #speak("Door is closing")
+            
+
+
+
+
+        else:
+            speak("Face is not matching with database. Entry denied")
+
+
+ 
+    cv2.imshow('Webcam',img)
+    cv2.waitKey(1)
